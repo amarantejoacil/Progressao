7ebfea623d2fccd6bf9a098aa62dca017401486b
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-29 18:56:37 -0400

    listview

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,4 +1,5 @@
-from cadastros.views import CampoCreate, CampoUpdate , CampoDelete, AtividadeCreate, AtividadeUpdate, AtividaDelete
+from cadastros.views import CampoCreate, CampoUpdate , CampoDelete, CampoList
+from cadastros.views import AtividadeCreate, AtividadeUpdate, AtividaDelete, AtividaList
 from django.urls import path
 #from .views import 
 
@@ -7,8 +8,10 @@ urlpatterns = [
     path('cadastrar/campo', CampoCreate.as_view(), name='cadastrar-campo'),
     path('editar/campo/<int:pk>/', CampoUpdate.as_view(), name='editar-campo'),
     path('excluir/campo/<int:pk>/', CampoDelete.as_view(), name='excluir-campo'),
+    path('listar/campo/', CampoList.as_view(), name='listar-campo'),
 
     path('cadastrar/atividade', AtividadeCreate.as_view(), name='cadastrar-atividade'), 
     path('editar/atividade/<int:pk>/', AtividadeUpdate.as_view(), name='editar-atividade'),   
     path('excluir/atividade/<int:pk>/', AtividaDelete.as_view(), name='excluir-atividade'),   
+    path('listar/atividade/', AtividaList.as_view(), name='listar-atividade'),   
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,47 +1,61 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from .models import Campo, Atividade
 
 from django.urls import reverse_lazy
 
 #CAMPO
+#CREATE
 class CampoCreate(CreateView):
     model = Campo
     fields = '__all__'
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listar-campo')
 
-
+#UPDATE
 class CampoUpdate(UpdateView):
     model = Campo
     fields = '__all__'
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')  
+    success_url = reverse_lazy('listar-campo')  
 
-
+#DELETE
 class CampoDelete(DeleteView):
     model = Campo   
     template_name = 'cadastros/form-excluir.html'
-    success_url = reverse_lazy('index')   
+    success_url = reverse_lazy('listar-campo')  
+
+#LIST
+class CampoList(ListView):
+    model = Campo   
+    template_name = 'cadastros/listas/campo-list.html'
 
 
 #ATIVIDADE
+#CREATE
 class AtividadeCreate(CreateView):
     model = Atividade
     fields = '__all__'
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
 
-
+#UPDATE
 class AtividadeUpdate(UpdateView):
     model = Atividade
     fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
 
-
+#DELETE
 class AtividaDelete(DeleteView):
     model = Atividade    
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('index')
+
+#LIST
+class AtividaList(ListView):
+    model = Atividade    
+    template_name = 'cadastros/listas/atividade-list.html'
+
 
