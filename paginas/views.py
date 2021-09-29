9e30ea305bc9cb41444274a96103a5dc86617713
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-29 10:44:18 -0400

    utilizando block e endblock

--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'modelo.html'
+
+
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
