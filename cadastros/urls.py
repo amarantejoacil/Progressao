18a39ab912aa9ecbb128945cb56c33072eb27b8c
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-29 17:13:56 -0400

    criando formulários

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,8 +1,9 @@
+from cadastros.views import CampoCreate, AtividadeCreate
 from django.urls import path
 #from .views import 
 
 
 urlpatterns = [
-    path('endereco', IndexView.as_view(), name='index'),
-    
+    path('cadastrar/campo', CampoCreate.as_view(), name='cadastrar-campo'),
+    path('cadastrar/atividade', AtividadeCreate.as_view(), name='cadastrar-atividade'),    
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from .models import Campo, Atividade
+
+from django.urls import reverse_lazy
+
+class CampoCreate(CreateView):
+    model = Campo
+    fields = '__all__'
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
 
 
+class AtividadeCreate(CreateView):
+    model = Atividade
+    fields = '__all__'
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
 
