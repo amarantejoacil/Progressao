9cc5a1f6c494e7e3cfe46a77ffd8d5c4e069eeb1
Joacil Amarante <joacil.amarante@gmail.com>
2021-10-01 13:19:38 -0400

    incluindo LoginRequiredMixin

--- cadastros/views.py
+++ cadastros/views.py
@@ -5,56 +5,68 @@ from .models import Campo, Atividade
 
 from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 #CAMPO
 #CREATE
-class CampoCreate(CreateView):
+class CampoCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Campo
     fields = '__all__'
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campo')
 
 #UPDATE
-class CampoUpdate(UpdateView):
+class CampoUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Campo
     fields = '__all__'
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campo')  
 
 #DELETE
-class CampoDelete(DeleteView):
+class CampoDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Campo   
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-campo')  
 
 #LIST
-class CampoList(ListView):
+class CampoList(LoginRequiredMixin, ListView):
+    #verificar se o usuario esta logado
+    login_url = reverse_lazy('login')
     model = Campo   
     template_name = 'cadastros/listas/campo-list.html'
+    
 
 
 #ATIVIDADE
 #CREATE
-class AtividadeCreate(CreateView):
+class AtividadeCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Atividade
     fields = '__all__'
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
 
 #UPDATE
-class AtividadeUpdate(UpdateView):
+class AtividadeUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Atividade
     fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
 
 #DELETE
-class AtividaDelete(DeleteView):
+class AtividaDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Atividade    
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('index')
 
 #LIST
-class AtividaList(ListView):
+class AtividaList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Atividade    
     template_name = 'cadastros/listas/atividade-list.html'
 
