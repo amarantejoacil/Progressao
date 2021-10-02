1b3f693a37bac9a6f17622a81e185dd482a27b2e
Joacil Amarante <joacil.amarante@gmail.com>
2021-10-02 16:23:27 -0400

    validação de e-mail cad usuario

--- usuarios/form.py
+++ usuarios/form.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import fields, models
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 class UsuarioForm(UserCreationForm):
     email = forms.EmailField(max_length=150)
@@ -11,3 +11,10 @@ class UsuarioForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        e = self.cleaned_data['email']
+        if User.objects.filter(email=e).exists():
+            raise ValidationError('Este e-mail {} já cadastrado!'.format(e))
+        
+        return e
