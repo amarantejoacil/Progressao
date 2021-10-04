0da34a1493e945667b4f27a9baf53c79546d21b3
Joacil Amarante <joacil.amarante@gmail.com>
2021-10-04 11:59:22 -0400

    aula 31 - editar os dados de perfil

--- paginas/views.py
+++ paginas/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'index2.html'
 
 
 class SobreView(TemplateView):
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,10 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User, Group
 from .form import UsuarioForm
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+
+from .models import Perfil
 
 
 class UsuarioCreate(CreateView):
@@ -20,6 +22,10 @@ class UsuarioCreate(CreateView):
         #depois de criar objeto
         self.object.groups.add(grupo)
         self.object.save()
+
+        Perfil.objects.create(usuario=self.object)
+
+
         return url
    
 
@@ -31,3 +37,24 @@ class UsuarioCreate(CreateView):
 
 
         return context
+
+
+class PerfilUpdate(UpdateView):
+    template_name = 'cadastros/form.html'
+    model = Perfil
+    fields = ['nome_completo', 'cpf', 'telefone']
+    success_url = reverse_lazy('index')
+
+    def get_object(self, queryset=None):
+        self.object  = get_object_or_404(Perfil, usuario=self.request.user)
+        return self.object
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['titulo'] = 'Meus dados'
+        context['botao'] = 'atualizados dados'
+
+        return context
+
+
